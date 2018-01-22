eab911ba79201a9f39a75c1670c955b17dc3e32e
abosma <atillabosma@hotmail.com>
2018-01-22 14:10:14 +0100

    throws SQLException nog toegevoegd

--- src/main/java/tosad/com/targetdbconnectionservices/ConnectionFacade.java
+++ src/main/java/tosad/com/targetdbconnectionservices/ConnectionFacade.java
@@ -1,16 +1,17 @@
 package tosad.com.targetdbconnectionservices;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class ConnectionFacade {
 	
 	private ConnectionController cc;
 	
-	public List<String> GetTableNames(int targetDBID){
+	public List<String> GetTableNames(int targetDBID) throws SQLException{
 		return cc.GetTableNames(targetDBID);
 	}
 	
-	public List<String> GetColumnNames(int targetDBID, String tableName){
+	public List<String> GetColumnNames(int targetDBID, String tableName) throws SQLException{
 		return cc.GetColumnNames(targetDBID, tableName);
 	}
 }
